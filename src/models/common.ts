f3506377038f29296c551e9c7b8c022587642120
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-10-24 15:24:31 +0700

    13 Setup APIs for student resource in react typescript

--- src/models/common.ts
+++ src/models/common.ts
@@ -8,3 +8,11 @@ export interface ListRespone<T> {
   data: T[];
   pagination: PaginationParams;
 }
+
+export interface ListParams {
+  _page: number;
+  _limit: number;
+  _sort: string;
+  _order: 'asc' | 'desc';
+  [key: string]: any;
+}
