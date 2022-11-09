3cd10f00e9209e6673ec3adb67e202a21f307bf6
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-11-09 10:46:46 +0700

    28 Route Student Feature

--- src/features/students/index.tsx
+++ src/features/students/index.tsx
@@ -1,5 +1,23 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import AddEditPage from './pages/AddEditPage';
+import ListPage from './pages/ListPage';
 
 export default function StudentFeature() {
-  return <div>StudentFeature</div>;
+  const match = useRouteMatch();
+
+  return (
+    <Switch>
+      <Route path={match.path} exact>
+        <ListPage></ListPage>
+      </Route>
+      <Route path={`${match.path}/add`}>
+        <AddEditPage></AddEditPage>
+      </Route>
+      <Route path={`${match.path}/:student`}>
+        <AddEditPage></AddEditPage>
+      </Route>
+    </Switch>
+  );
 }
---
+++ src/features/students/pages/ListPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ListPage() {
+  return <div>Student Feature</div>;
+}
